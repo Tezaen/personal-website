fdfdb516e943c9e36e548085ef8e1e74fbba64f8
Tezaen <Philippines420AD>
2021-07-15 16:57:19 -0700

    Added a register user route in case I want users for my website

--- client/src/components/projects/ProjectItem.js
+++ client/src/components/projects/ProjectItem.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
+import TimestampToDate from '../../utils/timestamp'
 //import ProjectContext from '../../context/project/projectContext';
 
 const ProjectItem = ({ project }) => {
     // destructure the project object
-    const { title, desc, datePosted } = project;
+    const { title, desc, datePosted, techs } = project;
 
-    const TimestampConvert = (timestamp) => {
-        var t = new Date(Date.UTC(1970, 0, 1)); // Epoch
-        t.setUTCSeconds(timestamp);
-        var n = t.toLocaleDateString();
-        return n;
-    }
+    // console.log("Title",title);
+    // console.log("Desc", desc);
+    // console.log("Date Posted", datePosted);
+    // console.log("Techs used", techs);
 
-    console.log(TimestampConvert(datePosted.seconds) );
-    const datePostedConverted = TimestampConvert(datePosted.seconds);
+    const datePostedConverted = TimestampToDate(datePosted.seconds);
     return (
-        <Card>
+        <Card className="my-2">
             <Card.Body>
                 <Card.Title className="display-4 text-center">
                     { title }
@@ -25,6 +23,15 @@ const ProjectItem = ({ project }) => {
                 <Card.Text>
                     { desc }
                 </Card.Text>
+                {
+                    techs !== undefined ? <Card.Text>
+                        Technologies: {
+                            techs.map(technology => 
+                                <span class="border mx-2 px-2">{technology}</span>
+                                )
+                        }
+                    </Card.Text> : <></>
+                }
                 Date Posted: { datePostedConverted }
             </Card.Body>
         </Card>
